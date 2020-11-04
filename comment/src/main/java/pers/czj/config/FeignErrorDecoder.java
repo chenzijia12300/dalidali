18d15b2cf0916e29d6e52b033d16902ccfe2808e
陈梓佳 <771240658@qq.com>
2020-11-04 20:33:35 +0800

    添加： 添加了feign全局异常捕捉器 修补： 修补了获得评论列表的错误SQL语句

---
+++ comment/src/main/java/pers/czj/config/FeignErrorDecoder.java
@@ -0,0 +1,49 @@
+package pers.czj.config;
+
+import cn.hutool.core.util.StrUtil;
+import feign.Body;
+import feign.Response;
+import feign.Util;
+import feign.codec.ErrorDecoder;
+import jdk.nashorn.internal.runtime.regexp.joni.exception.InternalException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ObjectUtils;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+/**
+ * 创建在 2020/11/4 18:50
+ */
+@Configuration
+public class FeignErrorDecoder implements ErrorDecoder {
+
+
+    public FeignErrorDecoder() {
+        log.info("feign全局创建");
+    }
+
+    private static final Logger log = LoggerFactory.getLogger(FeignErrorDecoder.class);
+
+    @Override
+    public Exception decode(String s, Response response) {
+        log.info("s:{},response:{}",s,response);
+        Response.Body body = response.body();
+        String bodyStr = null;
+        if (!ObjectUtils.isEmpty(body)){
+            try {
+                bodyStr = Util.toString(body.asReader(Charset.forName("UTF-8")));
+                log.info("bodyStr:{}",bodyStr);
+                if (StrUtil.isNotBlank(bodyStr)){
+                    // DO SOME THING
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return new InternalException(bodyStr);
+    }
+}
