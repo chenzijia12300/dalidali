afc192bdf6dc03f61e9e9fa505557d6d8ba0bb44
陈梓佳 <771240658@qq.com>
2022-07-10 14:48:26 +0800

    feat: 提交评论模块pom.xml

---
+++ comment/src/test/java/pers/czj/CommentApplicationTests.java
@@ -0,0 +1,27 @@
+package pers.czj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.ObjectUtils;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@SpringBootTest
+class CommentApplicationTests {
+
+    @Test
+    void contextLoads() {
+        List list = new ArrayList<>();
+        list.forEach(System.out::println);
+    }
+
+    public static void main(String[] args) {
+//        Collections.synchronizedMap()
+    }
+
+}
